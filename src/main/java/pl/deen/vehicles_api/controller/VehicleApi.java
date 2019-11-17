eaa3fa8f01f8a1ab7097c0cf572426acc587bcb6
Dominik <dominik-nowak@hotmail.com>
2019-11-17 20:48:05 +0100

    +split VehicleApi into VehicleService

--- src/main/java/pl/deen/vehicles_api/controller/VehicleApi.java
+++ src/main/java/pl/deen/vehicles_api/controller/VehicleApi.java
@@ -1,44 +1,41 @@
 package pl.deen.vehicles_api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.deen.vehicles_api.model.Vehicle;
+import pl.deen.vehicles_api.service.VehicleService;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/vehicles", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+@RequestMapping(value = "/vehicles", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 public class VehicleApi {
-    private List<Vehicle> vehicleList;
 
-    public VehicleApi() {
-        this.vehicleList = new ArrayList<>();
-        vehicleList.add(new Vehicle(1L, "Volkswagen", "Passat", "black"));
-        vehicleList.add(new Vehicle(2L, "Honda", "Civic", "black"));
-        vehicleList.add(new Vehicle(3L, "Peugeot", "307", "red"));
+    private VehicleService vehicleService;
+
+    @Autowired
+    public VehicleApi(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
     }
 
     @GetMapping
     public List<Vehicle> getVehicleList() {
-        return vehicleList;
+        return vehicleService.getVehicleList();
     }
 
     @GetMapping("/id/{id}")
-    public ResponseEntity getVehicleById(@PathVariable("id") long id) {
-        Optional<Vehicle> first = vehicleList.stream().filter(vehicle -> vehicle.getId() == id).findFirst();
-        return first.map(vehicle -> new ResponseEntity(vehicle, HttpStatus.OK)).orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+    public ResponseEntity<Vehicle> getVehicleById(@PathVariable("id") long id) {
+        return vehicleService.getVehicleById(id).map(vehicle -> new ResponseEntity(vehicle, HttpStatus.OK)).orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/color/{color}")
-    public ResponseEntity getVehiclesByColor(@PathVariable("color") String color) {
-        List<Vehicle> vehicles = vehicleList.stream().filter(vehicle -> vehicle.getColor().equalsIgnoreCase(color)).collect(Collectors.toList());
-        if (!vehicles.isEmpty()) {
-            return new ResponseEntity(vehicles, HttpStatus.OK);
+    public ResponseEntity<List<Vehicle>> getVehiclesByColor(@PathVariable("color") String color) {
+        List<Vehicle> vehiclesByColor = vehicleService.getVehiclesByColor(color);
+        if (!vehiclesByColor.isEmpty()) {
+            return new ResponseEntity(vehiclesByColor, HttpStatus.OK);
         }
         return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
@@ -46,20 +43,15 @@ public class VehicleApi {
 
     @PostMapping
     public ResponseEntity addVehicle(@RequestBody Vehicle newVehicle) {
-        Optional<Vehicle> first = vehicleList.stream().filter(vehicle -> vehicle.getId() == newVehicle.getId()).findFirst();
-        if (first.isPresent()) {
-            return new ResponseEntity(HttpStatus.IM_USED);
+        if (vehicleService.addVehicle(newVehicle)) {
+            return new ResponseEntity(HttpStatus.CREATED);
         }
-        vehicleList.add(newVehicle);
-        return new ResponseEntity(HttpStatus.CREATED);
+        return new ResponseEntity(HttpStatus.IM_USED);
     }
 
     @PutMapping
     public ResponseEntity modVehicle(@RequestBody Vehicle newVehicle) {
-        Optional<Vehicle> first = vehicleList.stream().filter(vehicle -> vehicle.getId() == newVehicle.getId()).findFirst();
-        if (first.isPresent()) {
-            vehicleList.remove(first.get());
-            vehicleList.add(newVehicle);
+        if (vehicleService.modVehicle(newVehicle)) {
             return new ResponseEntity(HttpStatus.OK);
         }
         return new ResponseEntity(HttpStatus.NOT_FOUND);
@@ -67,9 +59,7 @@ public class VehicleApi {
 
     @PatchMapping("/{id}")
     public ResponseEntity patchVehicleColor(@RequestBody String updatedColor, @PathVariable("id") long id) {
-        Optional<Vehicle> first = vehicleList.stream().filter(vehicle -> vehicle.getId() == id).findFirst();
-        if (first.isPresent()) {
-            first.get().setColor(updatedColor);
+        if (vehicleService.patchVehicleColor(updatedColor,id)) {
             return new ResponseEntity(HttpStatus.OK);
         }
         return new ResponseEntity(HttpStatus.NOT_FOUND);
@@ -77,10 +67,8 @@ public class VehicleApi {
 
     @DeleteMapping("/{id}")
     public ResponseEntity removeVehicle(@PathVariable("id") long id) {
-        Optional<Vehicle> first = vehicleList.stream().filter(vehicle -> vehicle.getId() == id).findFirst();
-        if (first.isPresent()) {
-            vehicleList.remove(first.get());
-            return new ResponseEntity(first.get(), HttpStatus.OK);
+        if (vehicleService.removeVehicle(id)) {
+            return new ResponseEntity(HttpStatus.OK);
         }
         return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
--- src/main/java/pl/deen/vehicles_api/service/VehicleService.java
+++ src/main/java/pl/deen/vehicles_api/service/VehicleService.java
@@ -1,7 +1,64 @@
 package pl.deen.vehicles_api.service;
 
 import org.springframework.stereotype.Service;
+import pl.deen.vehicles_api.model.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class VehicleService {
+    private List<Vehicle> vehicleList;
+
+    public VehicleService() {
+        this.vehicleList = new ArrayList<>();
+        vehicleList.add(new Vehicle(1L, "Volkswagen", "Passat", "black"));
+        vehicleList.add(new Vehicle(2L, "Honda", "Civic", "black"));
+        vehicleList.add(new Vehicle(3L, "Peugeot", "307", "red"));
+    }
+
+    public List<Vehicle> getVehicleList() {
+        return vehicleList;
+    }
+
+    public Optional<Vehicle> getVehicleById(long id) {
+        return vehicleList.stream().filter(vehicle -> vehicle.getId() == id).findFirst();
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color) {
+        return vehicleList.stream().filter(vehicle -> vehicle.getColor().equalsIgnoreCase(color)).collect(Collectors.toList());
+    }
+
+    public boolean addVehicle(Vehicle newVehicle) {
+        Optional<Vehicle> vehicleOptional = vehicleList.stream().filter(vehicle -> vehicle.getId() == newVehicle.getId()).findFirst();
+        if (vehicleOptional.isPresent()){
+            return false;
+        }
+        return vehicleList.add(newVehicle);
+    }
+
+    public boolean modVehicle(Vehicle newVehicle){
+        Optional<Vehicle> optionalVehicle = vehicleList.stream().filter(vehicle -> vehicle.getId() == newVehicle.getId()).findFirst();
+        if (optionalVehicle.isPresent()){
+            vehicleList.remove(optionalVehicle.get());
+            return vehicleList.add(newVehicle);
+        }
+        return false;
+    }
+
+    public boolean patchVehicleColor(String updatedColor, long id){
+        Optional<Vehicle> optionalVehicle = vehicleList.stream().filter(vehicle -> vehicle.getId() == id).findFirst();
+        if (optionalVehicle.isPresent()){
+            optionalVehicle.get().setColor(updatedColor);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeVehicle(long id){
+        Optional<Vehicle> optionalVehicle = vehicleList.stream().filter(vehicle -> vehicle.getId() == id).findFirst();
+        return optionalVehicle.map(vehicle -> vehicleList.remove(vehicle)).orElse(false);
+    }
 }
